b416d89c7b1b23e90265ccbf981ed38bcdef3941
JoaoLucasRB <32493395+JoaoLucasRB@users.noreply.github.com>
2017-10-19 17:24:47 -0200

    Create CatalProdutos.java

---
+++ CatalProdutos.java
@@ -0,0 +1,26 @@
+
+package sistemamercado;
+import java.util.*;
+
+public class CatalProdutos {
+    EspecProduto spec;
+    ItemDeVenda iDV;
+    Integer id;
+    String desc;
+    Float preco;
+    
+    public CatalProdutos(EspecProduto spec,ItemDeVenda item){
+        this.spec = spec;
+        this.iDV = item;
+    }
+    
+    public void encontrar(int id){
+        
+        if(spec.idDesc.containsKey(id)){
+            iDV.desc = spec.idDesc.get(id);
+            iDV.preco = spec.descPreco.get(spec.idDesc.get(id));
+            iDV.id = id;
+        }
+    }
+    
+}
