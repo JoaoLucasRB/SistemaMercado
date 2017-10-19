e878b1fd27c4df8aecb743dca4cb204335708543
JoaoLucasRB <32493395+JoaoLucasRB@users.noreply.github.com>
2017-10-19 17:23:59 -0200

    Create Venda.java

---
+++ Venda.java
@@ -0,0 +1,26 @@
+
+package sistemamercado;
+import java.util.*;
+
+public class Venda {
+    EspecProduto spec;
+    Scanner ent = new Scanner(System.in);
+    ArrayList listaProdutos = new ArrayList();
+    public Venda(EspecProduto spec){
+        this.spec = spec;
+    }
+    
+    public void incluirItemDeVenda(){
+        Scanner ent = new Scanner(System.in);
+        int id = ent.nextInt();
+        ItemDeVenda item = new ItemDeVenda();
+        CatalProdutos catal = new CatalProdutos(spec,item);
+        catal.encontrar(id);
+        listaProdutos.add(item);
+        Iterator i = listaProdutos.iterator();
+        while(i.hasNext()){
+            ItemDeVenda proximoItem = (ItemDeVenda) i.next();
+            System.out.println(proximoItem.toString());
+        }
+    }
+    
