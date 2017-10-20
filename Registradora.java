d172a69495268faca5376d429f607afed9281fb5
JoaoLucasRB <32493395+JoaoLucasRB@users.noreply.github.com>
2017-10-19 23:11:22 -0200

    Update Registradora.java

--- Registradora.java
+++ Registradora.java
@@ -1,12 +1,68 @@
 
 package sistemamercado;
-
+import java.util.*;
 
 public class Registradora {
     EspecProduto spec = new EspecProduto();
     Venda venda;
-    public void novaVenda(){
-        venda = new Venda(spec);
-        venda.incluirItemDeVenda();
+    Registradora reg;
+    
+    
+    public Registradora(){
+        this.reg = reg;
+    }
+    public void menuRegistradora(){    
+        try{
+            Scanner ent = new Scanner(System.in);
+            System.out.println("\n1. Nova Venda");
+            System.out.println("0. Encerrar Programa");
+            System.out.println("Opção: ");
+            int id = ent.nextInt();
+            switch(id){
+                case 1:{
+                    venda = new Venda(spec,reg);
+                    this.menuVenda();
+                }
+                case 0:{
+                    System.exit(0);
+                }
+                default:{
+                    System.out.println("Opção inválida. Tente Novamente.");
+                }
+            }
+        }catch(InputMismatchException e){
+            System.out.println("Por favor. use apenas números no menu");
+            this.menuVenda();
+        }
+    }
+    
+    public void menuVenda(){
+        int id = 0;
+        Scanner ent = new Scanner(System.in);
+        System.out.println("\nTotal: " + venda.Total);
+        System.out.println("1. Incluir novo Item");
+        System.out.println("2. Encerrar Venda");
+        try{
+                System.out.println("Opcao: ");
+                id = ent.nextInt();
+            
+            switch(id){
+                case 1: {
+                    venda.incluirItemDeVenda();
+                    this.menuVenda();
+                }
+                case 2:{
+                    venda.encerrarVenda();
+                    this.menuRegistradora();
+                }
+                default:{
+                    System.out.println("Insira uma opção válida.");
+                    this.menuVenda();
+                }
+            }
+        }catch(InputMismatchException e){
+            System.out.println("Por favor. use apenas números no menu");
+            this.menuVenda();
+        }
     }
 }
